c1b3577d99364e4fd2fd7f0a4f4573a903611ca4
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-04-07 18:09:10 -0300

    Add links to the page component

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,22 @@ export const runtime = "edge";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <p className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-lg">
-          A new portfolio is under construction. 🏗️
-        </p>
+        <div className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-lg text-center">
+          <p className="text-xl">A new portfolio is under construction. 🏗️</p>
+          <br />
+          <p>Check my online profiles:</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
+      <div className="flex flex-col w-full h-full grow items-center">
+        <div id="LinksList" className="flex grow max-h-96 text-center">
+          <div id="SocialLink" className="flex flex-col justify-around">
+            <a href="https://linkedin.com/in/muriloloffi">LinkedIn</a>
+            <a href="https://mastodon.social/@muriloloffi">Mastodon</a>
+            <a href="https://github.com/muriloloffi">Github</a>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
